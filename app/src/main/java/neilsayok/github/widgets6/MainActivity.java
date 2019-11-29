b7a595f9effe19c0cdc071643139884395d73934
PUBGautoBOT <bot.pubgfinalproj@gmail.com>
2019-11-29 10:15:36 +0530

    Detect Face Widget Added

--- app/src/main/java/neilsayok/github/widgets6/MainActivity.java
+++ app/src/main/java/neilsayok/github/widgets6/MainActivity.java
@@ -3,6 +3,9 @@ package neilsayok.github.widgets6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import neilsayok.github.attendancewidgets.DetectFaceWidget;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final boolean[] warn = {true};
+
+        final DetectFaceWidget df = findViewById(R.id.df);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                df.setWarning(!warn[0]);
+                warn[0] = !warn[0];
+            }
+        });
     }
 }
